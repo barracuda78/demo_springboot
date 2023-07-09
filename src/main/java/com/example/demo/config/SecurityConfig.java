f2e4cc208e7eca33a25fcf0181c5b37ee077aeaa
Andrei Ruzaev <andrei_ruzaev@epam.com>
2023-07-09 18:02:05 +0300

    US000003: security: inmemory  userdetailsservice and nooppasswordencoder

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService getUserDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("barracuda")
+                .password("barracuda")
+                .authorities("all", "read", "write", "delete")
+                .build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
